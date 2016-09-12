35b52e96772212c06ffce92469fac5e746718fbe
Alexey Plutalov <demiazz.py@gmail.com>
2016-09-12 15:01:08 +0300

    Add aliases for `querySelector` and `querySelectorAll`

---
+++ spec/query-selector-alias.spec.js
@@ -0,0 +1,59 @@
+import application from 'lighty';
+
+import { fixture, clear } from './fixtures';
+
+import plugin from '../src/index';
+
+
+describe('lighty-plugin-base', () => {
+  beforeAll(() => {
+    application.use(plugin).run();
+  });
+
+  afterEach(clear);
+
+  describe('query selector alias', () => {
+    // eslint-disable-next-line
+    it('adds `this.querySelector(<selector>)` alias for `this.node.querySelector(<selector>)`', (done) => {
+      fixture(`
+        <div class="outside element">
+        <div class="query-selector">
+          <div class="inside element" />
+          <div>
+            <div class="inside element" />
+          </div>
+        </div>
+      `);
+
+      application.component('.query-selector', {
+        init() {
+          expect(this.querySelector('.element'))
+            .toEqual(document.querySelector('.query-selector .element'));
+
+          done();
+        },
+      }).vitalize();
+    });
+
+    it('adds `this.querySelectorAll(<selector>)` alias for `this.node.querySelectorAll(<selector>)`', (done) => {
+      fixture(`
+        <div class="outside element">
+        <div class="query-selector-all">
+          <div class="inside element" />
+          <div>
+            <div class="inside element" />
+          </div>
+        </div>
+      `);
+
+      application.component('.query-selector-all', {
+        init() {
+          expect(this.querySelectorAll('.element'))
+            .toEqual(document.querySelectorAll('.query-selector-all .element'));
+
+          done();
+        },
+      }).vitalize();
+    });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import { plugin } from 'lighty';
 function pluginInitializer() {
   return function transform(component, node) {
     component.node = node;
+
+    component.querySelector = function querySelector(selector) {
+      return this.node.querySelector(selector);
+    };
+
+    component.querySelectorAll = function querySelectorAll(selector) {
+      return this.node.querySelectorAll(selector);
+    };
   };
 }
 
